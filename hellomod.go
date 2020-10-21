e4b0594d47079967269243e39484c8a1215a3116
liuyuanlin <418079061@qq.com>
2020-10-21 11:54:17 +0800

    update hellmod to v1.0.1

--- hellomod.go
+++ hellomod.go
@@ -1,4 +1,3 @@
-/*的风俗的风俗第三方地方三*/
 package hellomod
 
 import (
@@ -6,5 +5,5 @@ import (
 )
 
 func SayHello(name string) string {
-	return fmt.Sprintf("hello,%s,我是依赖包hellomod", name)
+	return fmt.Sprintf("hello,%s,我是依赖包hellomod V1.0.1", name)
 }
